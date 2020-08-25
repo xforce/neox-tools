d0c7da0027f2bcbd5633d8659aec1519dede9f66
Alexander Guettler <alexander@guettler.io>
2020-08-25 07:31:12 -0700

    Fix some issues with decompile_pyc when called from somewhere else

--- scripts/decompile_pyc.py
+++ scripts/decompile_pyc.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+
+# NOTE(alexander): DO NOT REORDER THIS!
 import sys
-sys.path.insert(0, "scripts/python-uncompyle6")
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.insert(0, os.path.join(dir_path, "python-uncompyle6"))
 
 from uncompyle6.bin.uncompile import main_bin
 
