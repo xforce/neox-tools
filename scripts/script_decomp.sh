4d9325e209fe8198b2732442d787d6496cbc7b41
Blake Wyatt <nabakin@protonmail.ch>
2020-08-31 09:13:18 -0400

    Update script_decomp.sh

--- scripts/script_decomp.sh
+++ scripts/script_decomp.sh
@@ -13,12 +13,29 @@ do
     file="$(basename "$file")"
     echo "$((100*$counter/$filecount))% - $file"
     python2 scripts/script_redirect.py script_nxs/$file > script/temp/$file.out # Decrypts into nxs
+    if [ $? -ne 0 ]
+    then
+        echo "Failed...sad face. Copied to 'script/failed/'"
+        cp "script_nxs/$file" "script/failed"
+        counter=$(($counter+1))
+        continue
+    fi
     python2 scripts/pyc_decryptor.py script/temp/$file.out script/pyc/$file.pyc # Demangles opcodes of nxs into pyc
+    if [ $? -ne 0 ]
+    then
+        echo "Failed...sad face. Copied to 'script/failed/'"
+        cp "script_nxs/$file" "script/failed"
+        counter=$(($counter+1))
+        continue
+    fi
     python3 scripts/decompile_pyc.py -o script/out/$file.py script/pyc/$file.pyc 2> /dev/null # Decompiles pyc into py
     if [ $? -ne 0 ]
     then 
-        echo "Failed...sad face. Copied to script/failed"
+        echo "Failed...sad face. Copied to 'script/failed/'"
         cp "script_nxs/$file" "script/failed"
+        counter=$(($counter+1))
+        continue
+
         # echo "Trying pycdc"
         # pycdc/pycdc script/pyc/$file.pyc > script/out/$file.py
         # if [ $? -ne 0 ]
@@ -37,5 +54,4 @@ do
         mkdir -p script/layout/$file_dir
         cp script/out/$file.py script/layout/$file_name
     fi
-    counter=$(($counter+1))
 done
